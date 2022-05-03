f86c08bab5d6734e1bd0c75f6abb6c73948ae3a3
Vincent <Vincentapartgames@gmail.com>
2022-05-03 11:12:22 +0200

    Added DB access

--- Api/Controllers/BoardingPassController.cs
+++ Api/Controllers/BoardingPassController.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using App.Services;
+using Database;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Check_in.Controllers
@@ -11,8 +12,20 @@ namespace Check_in.Controllers
         [HttpPost(Name = "PostBoardingPass")]
         public BoardingPass Post([FromBody] BoardingPassString boardingPass)
         {
-            return new Parser().Decode(boardingPass.BoardingPass);
+            
+            BoardingPass boarding = new Parser().Decode(boardingPass.BoardingPass);
+            string fullName = new DbName().GetNames(boarding.OperatingCarrierPnrCode.Trim());
+            if (fullName != "Undefined")
+            {
+                boarding.PassengerName = fullName;
+            }
+            else
+            {
+                return new BoardingPass();
+            }
+            return boarding;
         }
+        
     }
 }
 
---
+++ Database/DbName.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Database;
+
+public class DbName
+{
+
+    public string GetNames(string pnrCode)
+    {
+        return GetName(pnrCode);
+    }
+    private string GetName(string pnrCode)
+    {
+        MySqlConnection con = new MySqlConnection("server=localhost;user=root;database=rotterdam;password=Vinnie11");
+        MySqlCommand cmd = new MySqlCommand("SELECT voornaam, achternaam FROM passagier WHERE PNR_code = @pnr", con);
+        cmd.Parameters.AddWithValue("@pnr", pnrCode);
+        con.Open();
+        MySqlDataReader reader;
+        reader = cmd.ExecuteReader();
+        string Name;
+        string fName;
+        string lName;
+        while (reader.Read())
+        {
+            fName = reader.GetString("voornaam");
+            lName = reader.GetString("achternaam");
+            Name = lName + "/" + fName;
+            return Name;
+        }
+
+        return "Undefined";
+
+    }
+}
