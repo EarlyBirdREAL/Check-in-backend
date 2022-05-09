4060b772314ee36a5df2ba0ae8fd94517b7763f2
Niek Otten <niekotten2003@gmail.com>
2022-05-09 09:53:09 +0200

    Improved unit tests

--- UnitTests/TestParser.cs
+++ UnitTests/TestParser.cs
@@ -7,88 +7,109 @@ namespace UnitTests;
 public class TestParser
 {
     private Parser _parser;
-    private BoardingPass _boardingPass;
+    private string _code;
     
     [SetUp]
     public void Setup()
     {
         _parser = new Parser();
         
-        var code =
-            "M1PEETERS/GEORDI      ESNT7OE AMSFRALH 0989 198M019B0067 35D>6180WW8197BLH              2A01651697905940 LH                        N*30600000K09         ";
-
-        _boardingPass = _parser.Decode(code);
+        _code = "M1PEETERS/GEORDI      ESNT7OE AMSFRALH 0989 198M019B0067 35D>6180WW8197BLH              2A01651697905940 LH                        N*30600000K09         ";
     }
 
     [Test]
     public void TestFormatCode()
     {
-        Assert.AreEqual(_boardingPass.FormatCode, "M");
+        var boardingPass = _parser.Decode(_code);
+        
+        Assert.AreEqual(boardingPass.FormatCode, "M");
     }    
     
     [Test]
     public void TestLegs()
     {
-        Assert.AreEqual(_boardingPass.Legs, 1);
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.Legs, 1);
     }  
     
     [Test]
     public void TestPassengerName()
     {
-        Assert.AreEqual(_boardingPass.PassengerName, "PEETERS/GEORDI");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.PassengerName, "PEETERS/GEORDI");
     }
 
     [Test]
     public void TestOperatingCarrierPnrCode()
     {
-        Assert.AreEqual(_boardingPass.OperatingCarrierPnrCode, "SNT7OE");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.OperatingCarrierPnrCode, "SNT7OE");
     }
     
     [Test]
     public void TestFromCityAirportCode()
     {
-        Assert.AreEqual(_boardingPass.FromCityAirportCode, "AMS");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.FromCityAirportCode, "AMS");
     }    
     
     [Test]
     public void TestToCityAirportCode()
     {
-        Assert.AreEqual(_boardingPass.ToCityAirportCode, "FRA");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.ToCityAirportCode, "FRA");
     }    
     
     [Test]
     public void TestOperatingCarrierDesignator()
     {
-        Assert.AreEqual(_boardingPass.OperatingCarrierDesignator, "LH");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.OperatingCarrierDesignator, "LH");
     }   
     
     [Test]
     public void TestFlightNumber()
     {
-        Assert.AreEqual(_boardingPass.FlightNumber, "0989");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.FlightNumber, "0989");
     }    
     
     [Test]
     public void TestDateOfFlight()
     {
-        Assert.AreEqual(_boardingPass.DateOfFlight, "198");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.DateOfFlight, "198");
     }   
     
     [Test]
     public void TestCompartmentCode()
     {
-        Assert.AreEqual(_boardingPass.CompartmentCode, "M");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.CompartmentCode, "M");
     }   
     
     [Test]
     public void TestSeatNumber()
     {
-        Assert.AreEqual(_boardingPass.SeatNumber, "019B");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.SeatNumber, "019B");
     }   
     
     [Test]
     public void TestCheckInSequenceNumber()
     {
-        Assert.AreEqual(_boardingPass.CheckInSequenceNumber, "0067");
+        var boardingPass = _parser.Decode(_code);
+
+        Assert.AreEqual(boardingPass.CheckInSequenceNumber, "0067");
     }
 }
